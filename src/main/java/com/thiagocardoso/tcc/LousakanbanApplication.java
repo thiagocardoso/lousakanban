77381fc241f5951aa376f7616da342aef6e3007e
Thiago Cardoso <thiagocardoso.s@gmail.com>
2015-10-18 13:46:14 -0200

    Initial commit

---
+++ src/main/java/com/thiagocardoso/tcc/LousakanbanApplication.java
@@ -0,0 +1,12 @@
+package com.thiagocardoso.tcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LousakanbanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LousakanbanApplication.class, args);
+    }
+}
