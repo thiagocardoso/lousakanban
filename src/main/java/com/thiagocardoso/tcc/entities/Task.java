4a16b0b59ae5fde74e62858eda897fa1fc386ef6
Thiago Cardoso <thiagocardoso.s@gmail.com>
2015-11-14 16:10:03 -0200

    Adicionado titulo a Task

--- src/main/java/com/thiagocardoso/tcc/entities/Task.java
+++ src/main/java/com/thiagocardoso/tcc/entities/Task.java
@@ -1,16 +1,20 @@
 package com.thiagocardoso.tcc.entities;
 
+import jersey.repackaged.com.google.common.base.Objects;
+
 public class Task {
 
 	private User user;
+	private String title;
 
-	Task(User user){
+	Task(User user, String title){
 		this.user = user;
+		this.title = title;
 		
 	}
 	
-	public static Task toUser(User user) {
-		return new Task(user);
+	public static Task toUser(User user, String title) {
+		return new Task(user, title);
 	}
 
 	public User getUser() {
@@ -22,4 +26,21 @@ public class Task {
 		return this;
 	}
 
+	public String getTitle() {
+		return this.title;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Task) {
+			final Task other = (Task)obj;
+			return Objects.equal(this.user, other.user) && Objects.equal(this.title, other.title);
+		}
+		return false;
+	}
+
+	@Override
+	public int hashCode() {
+		return super.hashCode();
+	}
 }
--- src/test/java/com/thiagocardoso/tcc/entities/TaskTest.java
+++ src/test/java/com/thiagocardoso/tcc/entities/TaskTest.java
@@ -5,22 +5,43 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TaskTest {
+	private static final String TITLE_TASK_1 = "Task#1";
+
 	@Test
 	public void createTaskToUser() {
-		assertNotNull(Task.toUser(User.from("test")));
+		final Task task = Task.toUser(User.from("test"), TITLE_TASK_1);
+		assertNotNull(task);
+		assertEquals(TITLE_TASK_1, task.getTitle());
 	}
 	
 	@Test
 	public void verifyTaskUser() {
-		assertEquals(User.from("test"), Task.toUser(User.from("test")).getUser());
+		assertEquals(User.from("test"), Task.toUser(User.from("test"), TITLE_TASK_1).getUser());
 	}
 	
 	@Test
 	public void createTaskToUserAndAssignToAnother(){
 		final User user1 = User.from("test");
 		final User user2 = User.from("another");
-		final Task task = Task.toUser(user1);
+		final Task task = Task.toUser(user1, TITLE_TASK_1);
 		assertEquals(user2, task.assign(user2).getUser());
+	}
+	
+	@Test
+	public void twoEqualTasksToSameUser() {
+		final User user1 = User.from("test");
+		final Task task1 = Task.toUser(user1, TITLE_TASK_1);
+		final Task task2 = Task.toUser(user1, TITLE_TASK_1);
+		assertEquals(task1, task2);
+	}
+	
+	@Test
+	public void twoEqualTasksToDifferentUser() {
+		final User user1 = User.from("test");
+		final User user2 = User.from("another");
+		final Task task1 = Task.toUser(user1, TITLE_TASK_1);
+		final Task task2 = Task.toUser(user2, TITLE_TASK_1);
+		assertNotEquals(task1, task2);
 	}
 	
 }
